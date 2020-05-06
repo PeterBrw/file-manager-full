c11c0ecbb7a15dbf0661cdad9c37a5b34ed67190
PeterBrw <peter.giurgiu@gmail.com>
2020-05-06 06:52:33 +0300

    finish-queries-and-mutations

--- server/src/schema/resolvers/File.js
+++ server/src/schema/resolvers/File.js
@@ -1,10 +1,93 @@
+const fs = require("fs");
+const path = require("path");
+const {
+    returnChildren,
+    returnName,
+    changeName,
+    deleteItem,
+    addItem,
+    dragAndDrop,
+} = require("../../models/logic");
+
 export const resolvers = {
-	Query: {
-		file: async (_, args, context) => {
-			return {
-				id: 1,
-				name: 'My Computer'
-			}
-		}
-	}
-}
+    Query: {
+        getFiles: (_, args, context) => {
+            const data = JSON.parse(
+                fs.readFileSync(
+                    path.join(__dirname, "../", "../", "models", "data.json")
+                )
+            );
+            return data;
+        },
+        getChildren: (_, { id }) => {
+            const data = JSON.parse(
+                fs.readFileSync(
+                    path.join(__dirname, "../", "../", "models", "data.json")
+                )
+            );
+            return returnChildren(data, id);
+        },
+        getName: (_, { id }) => {
+            const data = JSON.parse(
+                fs.readFileSync(
+                    path.join(__dirname, "../", "../", "models", "data.json")
+                )
+            );
+            return returnName(data, id);
+        },
+    },
+    Mutation: {
+        changeName: (_, { id, newName }) => {
+            const data = JSON.parse(
+                fs.readFileSync(
+                    path.join(__dirname, "../", "../", "models", "data.json")
+                )
+            );
+            const changeData = changeName(data, id, newName);
+            fs.writeFileSync(
+                path.join(__dirname, "../", "../", "models", "data.json"),
+                JSON.stringify(changeData)
+            );
+            return changeData;
+        },
+        deleteFile: (_, { id }) => {
+            const data = JSON.parse(
+                fs.readFileSync(
+                    path.join(__dirname, "../", "../", "models", "data.json")
+                )
+            );
+            const deleteData = deleteItem(data, id);
+            fs.writeFileSync(
+                path.join(__dirname, "../", "../", "models", "data.json"),
+                JSON.stringify(deleteData)
+            );
+            return deleteData;
+        },
+        addFile: (_, { id, name }) => {
+            const data = JSON.parse(
+                fs.readFileSync(
+                    path.join(__dirname, "../", "../", "models", "data.json")
+                )
+            );
+            const addedItem = addItem(data, id, name);
+            fs.writeFileSync(
+                path.join(__dirname, "../", "../", "models", "data.json"),
+                JSON.stringify(addedItem)
+            );
+            return addedItem;
+        },
+        dragFile: (_, { id, idTo }) => {
+            const data = JSON.parse(
+                fs.readFileSync(
+                    path.join(__dirname, "../", "../", "models", "data.json")
+                )
+            );
+            const dragData = dragAndDrop(data, id, idTo);
+            fs.writeFileSync(
+                path.join(__dirname, "../", "../", "models", "data.json"),
+                JSON.stringify(dragData)
+            );
+            return dragData;
+        },
+    },
+};
